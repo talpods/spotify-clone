a6373b9c54ac88c33fe82d9213fa8aaa63c8a76f
adonis butufei <adonis.butufei@gmail.com>
2021-04-13 17:41:39 +0300

    Add sourceMaps() for debugging and Disable mix notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,9 @@
 let mix = require('laravel-mix');
+mix.disableNotifications();
 
 mix.js('src/Js/app.js', 'js')
     .postCss("src/Css/app.css", "css", [
         require("tailwindcss"),
     ])
+    .sourceMaps()
     .setPublicPath('public');
