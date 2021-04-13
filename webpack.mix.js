b46fbeec20b77ee141703c869b6bb453cd130a54
adonis butufei <adonis.butufei@gmail.com>
2021-04-13 17:51:56 +0300

    use lower case names for folders

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
 mix.disableNotifications();
 
-mix.js('src/Js/app.js', 'js')
-    .postCss("src/Css/app.css", "css", [
+mix.js('src/js/app.js', 'js')
+    .postCss("src/css/app.css", "css", [
         require("tailwindcss"),
     ])
     .sourceMaps()
