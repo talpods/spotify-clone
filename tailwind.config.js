792821754bab5949488c5ec37d4f0692abe8eb00
adonis butufei <adonis.butufei@gmail.com>
2021-04-14 19:53:25 +0300

    disable purging for development purposes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: {
-    enabled: true,
+    // enabled: true,
     content: ["./public/*.html"]
   },
   darkMode: false, // or 'media' or 'class'
