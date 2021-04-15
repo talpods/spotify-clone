a61b4ad073648f5ef486f0b69e3c06cda394435b
adonis butufei <adonis.butufei@gmail.com>
2021-04-15 14:41:55 +0300

    responsive card collection

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,14 @@ module.exports = {
           toolbar: '#202020'
         },
         emerald: colors.emerald
-      }
+      },
+      gridTemplateColumns: {
+        'cards-2': 'repeat(2, 220px)',
+        'cards-3': 'repeat(3, 220px)',
+        'cards-4': 'repeat(4, 220px)',
+        'cards-6': 'repeat(6, 220px)',
+        'cards-7': 'repeat(7, 220px)',
+      },
     },
   },
   variants: {
