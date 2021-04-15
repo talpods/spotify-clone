6ddefca3526fddd6945e614d0c9f1c8e582848eb
adonis butufei <adonis.butufei@gmail.com>
2021-04-15 13:17:21 +0300

    Connect tracks

--- src/js/cards.js
+++ src/js/cards.js
@@ -3,18 +3,55 @@ import {getTracks, getCategories, search, getCategorySlug} from './lib/data';
 import Card from './components/Card';
 import CardCollection from './components/CardCollection';
 
-const container = document.querySelector("#demo");
+const context = {};
 
-
-(async () => {
+const fillCategories = async (container) => {
     const categories = await getCategories();
     const cards = new CardCollection("Categories");
 
     for(let category of categories) {
-        cards.append(new Card(category.title, category.slug, category.image));
+
+        const props = {
+            title: category.title,
+            text: category.slug,
+            image: category.image
+        };
+
+        cards.append(new Card(props));
     }
 
     cards.mount(container);
+
+    return categories;
+};
+
+const fillTracks = async (container) => {
+    const tracks = await getTracks();
+    const cards = new CardCollection("Trakcs");
+
+    for(let track of tracks) {
+        const props = {
+            title: track.title,
+            text: track.author,
+            image: track.image
+        }
+
+        cards.append(new Card(props));
+    }
+
+    cards.mount(container);
+
+    return tracks;
+};
+
+(async () => {
+
+    try {
+        context.categories = await fillCategories(document.querySelector("#categories"));
+        context.tracks = await fillTracks(document.querySelector("#tracks"));
+    } catch (error) {
+        console.log(error);
+    }
 })();
 
 
--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -5,14 +5,14 @@ import Component, {setImageSource, setValue} from '../lib/Component';
 import config from "./Card.json";
 
 class Card extends Component {
-    constructor(title, text, image) {
+    constructor(props) {
         // load structure
         super(config);
 
         // fill data
-        setValue(title, this.element.querySelector(".title"));
-        setValue(text, this.element.querySelector(".text"));
-        setImageSource(image, this.element.querySelector(".image"));
+        setValue(props.title, this.element.querySelector(".title"));
+        setValue(props.text, this.element.querySelector(".text"));
+        setImageSource(props.image, this.element.querySelector(".image"));
     }
 }
 
