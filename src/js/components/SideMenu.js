51d6422c0b54700edd56646ec5f975f228552b21
adonis butufei <adonis.butufei@gmail.com>
2021-04-16 00:16:14 +0300

    Add SideMenu

---
+++ src/js/components/SideMenu.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import Component from '../lib/Component';
+import Link from '../components/Link';
+
+import data from './SideMenu.json';
+
+class SideMenu extends Component {
+    constructor() {
+        super(data);
+
+        this.links = [
+            new Link({text: "Home", to: "home.html",img: "img/home.png"}),
+            new Link({text: "Search", to: "search.html",img: "img/search.png"}),
+            new Link({text: "Library",to: "playlist.html", img: "img/library.png"})
+        ];
+
+        this.links.forEach(crt => crt.mount(this.element));
+    }
+
+    highlight(name) {
+        this.links.forEach(crt => crt.highlight(crt.text === name));
+    }
+}
+
+export default SideMenu;
