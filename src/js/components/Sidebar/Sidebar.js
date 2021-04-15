2d6b183f550a5d127096edb41348587768b89500
adonis butufei <adonis.butufei@gmail.com>
2021-04-16 01:00:11 +0300

    connect sidebar

--- src/js/components/Sidebar/Sidebar.js
+++ src/js/components/Sidebar/Sidebar.js
@@ -1,11 +1,19 @@
 "use strict";
 
-import Component from '../lib/Component';
+import Component from '../../lib/Component';
 import data from './Sidebar.json';
+import SideMenu from './SideMenu';
+import SideLists from './SideLists';
 
 class Sidebar extends Component {
     constructor() {
         super(data);
+
+        this.menu = new SideMenu();
+        this.menu.mount(this.element);
+
+        this.lists = new SideLists();
+        this.lists.mount(this.element);
     }
 }
 
