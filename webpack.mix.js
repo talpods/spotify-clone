dbc6c52b0417e6055c5fb8a280f2e0a46d96e088
adonis butufei <adonis.butufei@gmail.com>
2021-04-15 12:39:00 +0300

    rename demo -> cards

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ let mix = require('laravel-mix');
 mix.disableNotifications();
 
 mix.js('src/js/app.js', 'js')
-   .js('src/js/demo.js', 'js')
+   .js('src/js/cards.js', 'js')
    .js('src/js/sidebar.js','js')
    .js('src/js/toolbar.js','js')
    .js('src/js/player.js','js')
