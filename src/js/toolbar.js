d0b3758ab5ff92bbaad7a7e0d95e1c8b295bbc5f
Farooqiese <farooq-2001@hotmail.com>
2021-04-16 03:10:48 +0400

    made the desgin for the toolbar at toolbar.html

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -1,4 +1,18 @@
 "use strict";
+import Toolbar from './components/Toolbar';
+let toolBarContainer = document.getElementById("toolbar");
+const tb = new Toolbar();
+tb.mount(toolBarContainer);
+
+
+
+
+
+
+
+
+
+
 
 
 // conbtains demo api for toolbar
