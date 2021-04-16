80fbd8c6ecf4f259f2ff64454f3607a3120943e6
adonis butufei <adonis.butufei@gmail.com>
2021-04-16 16:14:04 +0300

    move card mounting

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,55 +1,18 @@
 import { getTracks, getCategories, search, getCategorySlug } from './lib/data';
 
 import Card from './components/Card';
-import CardCollection from './components/CardCollection';
+import CardCollection, {mountCategories, mountTracks} from './components/CardCollection';
 
 // the context will be used later for play / stop events
 const context = {};
 
-const fillCategories = async (container) => {
-    const categories = await getCategories();
-    const cards = new CardCollection("Categories");
 
-    for (let category of categories) {
-
-        const props = {
-            title: category.title,
-            text: category.slug,
-            image: category.image
-        };
-
-        cards.append(new Card(props));
-    }
-
-    cards.mount(container);
-
-    return categories;
-};
-
-const fillTracks = async (container) => {
-    const tracks = await getTracks();
-    const cards = new CardCollection("Trakcs");
-
-    for (let track of tracks) {
-        const props = {
-            title: track.title,
-            text: track.author,
-            image: track.image
-        }
-
-        cards.append(new Card(props));
-    }
-
-    cards.mount(container);
-
-    return tracks;
-};
 
 (async () => {
 
     try {
-        context.categories = await fillCategories(document.querySelector("#categories"));
-        context.tracks = await fillTracks(document.querySelector("#tracks"));
+        context.categories = await mountCategories(document.querySelector("#categories"));
+        context.tracks = await mountTracks(document.querySelector("#tracks"));
 
     } catch (error) {
         console.log(error);
