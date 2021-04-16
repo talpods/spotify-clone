61cdbe9fa4002200216a5bf97cf44491dc5a1cd2
adonis butufei <adonis.butufei@gmail.com>
2021-04-16 14:09:29 +0300

    Add Player component

--- src/js/components/Player/Player.js
+++ src/js/components/Player/Player.js
@@ -2,11 +2,42 @@
 
 import Component from '../../lib/Component';
 import data from './Player.json';
+import PlayerImage from './PlayerImage';
+import PlayerAudio from './PlayerAudio';
+import PlayerInfo from './PlayerInfo';
+import Track from './Track';
+
 
 class Player extends Component {
-    constructor() {
+    constructor(props) {
         super(data);
+
+        this.image = new PlayerImage(props);
+        this.image.mount(this.element);
+
+        this.audio  = new PlayerAudio(props);
+        this.audio.mount(this.element);
+
+        this.info = new PlayerInfo(props);
+        this.info.mount(this.element);
+    }
+
+    update(data) {
+        this.image.image = data.image;
+        this.audio.song = data.song;
+        this.info.title = data.title;
+        this.info.author = data.author;
     }
 }
 
+const mountPlayer = (parent, props = new Track()) => {
+    const player = new Player(props);
+    player.mount(parent);
+    return player;
+};
+
 export default Player;
+
+export {
+    mountPlayer
+};
