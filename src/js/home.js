152b0b1cdb7c21fe06f68efeaea48ebcc364a265
adonis butufei <adonis.butufei@gmail.com>
2021-04-16 19:58:24 +0300

    first version of home page

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,40 @@
 "use strict";
 
-console.log("home page");
+import {mountSidebar} from "./components/Sidebar/Sidebar";
+import {mountCategories, mountTracks} from './components/CardCollection';
+import {mountPlayer} from './components/Player/Player'
+import {Track} from './components/Player/Track';
+
+//import {mount}
+
+import Toolbar from './components/Toolbar';
+
+// TODO: move to Toolbar component
+const mountToolbar = (container) => {
+    let toolBarContainer = document.getElementById("toolbar");
+    const tb = new Toolbar();
+    tb.mount(toolBarContainer);
+    return toolBarContainer;
+};
+
+const context = {};
+
+// select containers
+const sidebarContainer = document.querySelector(".sidebar-container");
+const  toolbarContainer = document.querySelector(".toolbar-container");
+const  categoriesContainer = document.querySelector(".categories-container");
+const  traksContainer = document.querySelector(".traks-container");
+const  playlistContainer = document.querySelector(".playlist-container");
+const  palyerContainer = document.querySelector(".palyer-container");
+
+
+(async () => {
+    context.sidebar = mountSidebar(sidebarContainer);
+    //context.toolbar = mountToolbar(toolbarContainer);
+
+    context.categories = mountCategories(categoriesContainer);
+    context.tracks = mountTracks(traksContainer);
+
+    context.palyer = mountPlayer(palyerContainer);
+
+})();
