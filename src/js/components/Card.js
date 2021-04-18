f91b6e7a73d6d94af60b3d9dce0e73a1f692b63f
adonis butufei <adonis.butufei@gmail.com>
2021-04-19 02:32:07 +0300

    Save props in parent (required for event handling). Add click functionality and augument event with props.

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -10,12 +10,33 @@ class Card extends Component {
         // load structure
         const config = isAuthenticated() ? data.auth : data.default;
 
-        super(config);
+        super(config, props);
 
         // fill data
         setValue(props.title, this.element.querySelector(".title"));
         setValue(props.text, this.element.querySelector(".text"));
         setImageSource(props.image, this.element.querySelector(".image"));
+
+        if(isAuthenticated()) {
+            this.listener = this.addListener("click", this.click, this);
+        }
+    }
+
+    // Disconnect listeners
+    removeEvents() {
+        this.removeListener("click", this.listener);
+    }
+
+    click(event) {
+        // filter clicks form the button and let other events to pass
+        // use optional chaining see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+        if(event.target?.parentElement?.dataset?.comment !== "play-btn") {
+            return;
+        }
+
+        // add card index to the event to identify which card was clicked
+        event.cardIndex = event.currentTarget?.dataset?.index;
+        event.props = this.props;
     }
 }
 
