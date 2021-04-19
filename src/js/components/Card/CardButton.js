26b992a2c5193828a66d159f96760a3ea8549921
adonis butufei <adonis.butufei@gmail.com>
2021-04-19 15:31:25 +0300

    Simplify button

--- src/js/components/Card/CardButton.js
+++ src/js/components/Card/CardButton.js
@@ -3,38 +3,15 @@ import data from './CardButton.json';
 
 import ButtonIcon from './ButtonIcon';
 
-const defaultProps = {
-    state1: {
-        image: "img/buttonplay.png",
-        className: "state1"
-    },
-    state2: {
-        image: "img/buttonpause.png",
-        className: "state2"
-    }
+const image = {
+    image: "img/buttonplay.png",
+    className: "state1"
 };
 
 class CardButton extends Component {
-    constructor(props = defaultProps)
-    {
+    constructor(props = image) {
         super(data);
-
-        this.state1 = new ButtonIcon(props.state1);
-        this.append(this.state1);
-
-        this.state2 = new ButtonIcon(props.state2);
-        this.append(this.state2);
-
-        // we need the handler to set active state
-        this.handler = this.addListener("click", this.click, this);
-        this.active = false;
-
-    }
-
-    click(event) {
-        this.active = !this.active;
-        this.element.classList.toggle("active");
-        event.props = {active: this.active};
+        this.append(new ButtonIcon(props));
     }
 }
 
